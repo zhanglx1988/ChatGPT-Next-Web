f770bd4537284b42bc7092a5ef02f9ef86908b33
z <zhanglongxin1988@163.com>
2023-05-17 09:53:36 +0800

    修改名称

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,7 +119,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT WEB</div>
+        <div className={styles["sidebar-title"]}>Large AI Model</div>
         <div className={styles["sidebar-sub-title"]}></div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
